51390b65606479fa2630578cf73fa2892758603c
Neeyat Lotlikar <neeyat.lotlikar@gmail.com>
2022-04-08 18:56:11 +0530

    Uploading Project

---
+++ wordcount/views.py
@@ -0,0 +1,42 @@
+import operator
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def homepage(request):
+    # data = {
+    #     'q1': "What's there for dinner?",
+    #     'q2': "Should we swiggy?",
+    # }
+    return render(request, 'index.html')#, data)
+
+
+def lemons(request):
+    response_str = "When life gives you lemon, make lemonade. - Boring Quote"
+    return HttpResponse(response_str)
+
+
+def count(req):
+    alltext = req.GET['alltext']
+    words = alltext.split()
+    wordcount = dict()
+    
+    for word in words:
+        if word in wordcount:
+            wordcount[word] += 1
+        else:
+            wordcount[word] = 1
+    
+    # sort the list in reverse of the natural order of the values
+    countedwords = sorted(wordcount.items(), key=operator.itemgetter(1), reverse=True)
+    
+    data = {
+        'totcount': len(words),
+        'alltext': alltext,
+        'wordcount': countedwords,
+    }
+    return render(req, 'count.html', data)
+
+
+def about(req):
+    return render(req, 'about.html')
